f8b51d3cedfd2365006513f6281fbae45263e6de
Werner <wstablein@hotmail.com>
2025-09-27 18:18:26 +0200

    Change of secret key to os.environ.get()

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -18,7 +18,7 @@ env = environ.Env()
 print("DJANGO_SECRET_KEY from os.environ:", os.environ.get("DJANGO_SECRET_KEY"))
 
 
-# # commented out for production
+ # commented out for production
 # environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,10 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-secret")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DJANGO_DEBUG")
+DEBUG = os.environ.get("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = []
 
